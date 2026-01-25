fd7a9109aac9bd64293b824ba39f68de7900bb43
Doniyor <ddonierov95@gmail.com>
2026-01-25 07:38:53 +0500

    production

--- youtube/build.sh
+++ youtube/build.sh
@@ -5,8 +5,6 @@ set -o errexit
 # Install dependencies (modify as needed for pip, poetry, etc.)
 pip install -r requirements.txt
 
-# Convert static asset files
-python manage.py collectstatic
 
 # Apply database migrations
 python manage.py migrate
